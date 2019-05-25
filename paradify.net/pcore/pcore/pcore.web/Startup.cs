1f2b2c5418f05dc7a61d148cfe93210a63c6c3cc
Volkan Akin <>
2019-05-25 21:32:11 +0200

    UIgnore the rest files

---
+++ paradify.net/pcore/pcore/pcore.web/Startup.cs
@@ -0,0 +1,79 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using web.Filters;
+using web.Services;
+using web.Services.Implementations;
+
+namespace pcore.web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                // Make the session cookie essential
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddHttpContextAccessor();
+            services.AddScoped<ITokenCookieService, TokenCookieService>();
+            services.AddScoped<IParadifyService, ParadifyService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ISessionService, SessionService>();
+            services.AddScoped<IPlaylistService, PlaylistService>();
+
+            services.AddMvc();
+
+            services.AddScoped<CustomHandleError>();
+            services.AddScoped<FilterClientToken>();
+            services.AddScoped<FilterUserToken>();
+            services.AddScoped<FilterUserTokenMust>();
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
